5274dc766a894626116a68ac94d6d91ec254ed04
Vasiliy Morkovkin <1istoobig@gmail.com>
2019-02-16 18:43:35 +0300

    django to flask

--- buycycle/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from buycycle.views import HomePageView
-
-urlpatterns = [
-    path('', HomePageView.as_view()),
-]
--- buycycle/wsgi.py
+++ buycycle/wsgi.py
@@ -1,16 +1,4 @@
-"""
-WSGI config for buycycle project.
+from buycycle import app
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buycycle.settings')
-
-application = get_wsgi_application()
+if __name__ == "__main__":
+    app.run()
