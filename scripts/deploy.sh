12536594d08b6c3cba646733a861caca2c399454
Vasiliy Morkovkin <1istoobig@gmail.com>
2019-03-26 01:08:55 +0300

    log folder in Travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ root@$SERVER_ADDR:/opt/buycycle
-ssh root@$SERVER_ADDR 'mkdir /opt/buycycle/logs'
+ssh root@$SERVER_ADDR 'mkdir /opt/buycycle/logs/logs'
 ssh root@$SERVER_ADDR 'systemctl restart gunicorn'
