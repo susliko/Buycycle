7ce0aa916256622f02e1ed5016fbd4612dad6f01
susliko <1istoobig@gmail.com>
2019-05-05 16:23:57 +0300

    install pip requirements

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ root@$SERVER_ADDR:/opt/buycycle
 ssh root@$SERVER_ADDR 'mkdir /opt/buycycle/buycycle/logs'
-ssh root@$SERVER_ADDR 'pip3 install /opt/buycycle/requirements.txt'
+ssh root@$SERVER_ADDR 'pip3 install -r /opt/buycycle/requirements.txt'
 ssh root@$SERVER_ADDR 'systemctl restart gunicorn'
