a82725727d2691c25a2578bb1b5e2d05c98a01a4
susliko <1istoobig@gmail.com>
2019-05-06 15:35:58 +0300

    change directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ root@$SERVER_ADDR:/home/buycycle
+
 ssh buycycle@$SERVER_ADDR 'mkdir /home/buycycle/buycycle/logs'
 ssh root@$SERVER_ADDR 'pip3 install -r /opt/buycycle/requirements.txt'
 ssh root@$SERVER_ADDR 'systemctl restart gunicorn'
