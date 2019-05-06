d962d3e3aabd3957c77fd4414ed07b3059960489
susliko <1istoobig@gmail.com>
2019-05-06 19:39:31 +0300

    deploy.sh modified

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ buycycle@$SERVER_ADDR:/home/buycycle
-ssh buycycle@$SERVER_ADDR 'mkdir /home/buycycle/buycycle/logs'
-ssh root@$SERVER_ADDR 'pip3 install -r /opt/buycycle/requirements.txt'
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ buycycle@$SERVER_ADDR:/home/buycycle/buycycle
+ssh buycycle@$SERVER_ADDR 'mkdir /home/buycycle/buycycle/buycycle/logs'
+ssh root@$SERVER_ADDR 'pip3 install -r /home/buycycle/buycycle/requirements.txt'
 ssh root@$SERVER_ADDR 'systemctl restart gunicorn'
